5eacdd6edae090968691dab862af91bbda4dfbf4
ChristopherM100 <christophermo100@gmail.com>
2025-04-19 19:53:03 -0700

    Implimented some items in checkout page like navbar information, order summary, pay for shipping button, and small text under the button

--- src/CheckoutPage.js
+++ src/CheckoutPage.js
@@ -6,10 +6,23 @@ function CheckoutPage() {
     <div>
       <div className="top-container">
         <img src="/TeslaLogo.png" alt="Tesla Logo" className="logo" />
+        <div className ="top-row-info">
+          <p>Model S</p>
+          <p>Model 3</p>
+          <p>Model X</p>
+          <p>Model Y</p>
+        </div>
+        <div className="top-account-text">
+          <p>Account</p>
+        </div>
+      </div>
+      <div className="shipping-payment-text">
+        <h1>Shipping Payment</h1>
+      </div>
+      <div className = "description-text">
+        <p>Complete payment for shipping your Tesla parts or accessories.</p>
       </div>
       <div className="main-container">
-      <h1>Shipping Payment</h1>
-        <h2>Complete payment for shipping your Tesla parts or accessories.</h2>
         <div className="top-row">
           <div className="card shipping-card">
             <h1>Shipping Information</h1>
@@ -17,10 +30,42 @@ function CheckoutPage() {
           </div>
 
           <div className="card order-card">
-            <h1>Order Summary</h1>
-            <p>Items, prices, totals</p>
+            <div className="order-summary">
+              <h1>Order Summary</h1>
+            </div>
+            <div className="order-row">
+              <div className = "order-text"> {/*left side of order summary*/}
+                <p>Shipping</p>
+                <p>Handling</p>
+                <p>Tax</p>
+              </div>
+              <div className = "order-textBlack"> {/*left side of order summary*/}
+                <p>$9.99</p>
+                <p>$2.00</p>
+                <p>$0.96</p>
+              </div>
+            </div>
+            <div>
+            <div className="line"></div>
+            </div>
+            <div className="order-row">
+              <div className = "order-textTotal">
+                <p>Total</p>
+              </div>
+              <div className = "order-textTotalNum"> {/*left side of order summary*/}
+                  <p>$12.95</p>
+              </div>
+            </div>
           </div>
+          <button type="submit" className="sign-in-button ">Pay for Shipping</button>
         </div>
+        <div className = "privacy-text">
+            <p>
+              By completing this payment, you agree to Tesla's
+              <span className="blue-text"> Terms of Use</span> and 
+              <span className="blue-text"> Privacy Policy</span>.
+            </p>
+          </div>
 
         <div className="card shipping-card">
           <h1>Shipping Method</h1>
